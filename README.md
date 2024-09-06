1a69ba3adee78178ed9a746bab97e1586273da13
Sumith Kumar Saini <sumithsaini01@gmail.com>
2024-09-06 19:38:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Clone of Apple website with client-side.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-zinc-900 text-white text-9xl w-full min-h-screen font-inter leading-none">
+      Project initialization.
+    </div>
+  );
+};
+
+export default App;
